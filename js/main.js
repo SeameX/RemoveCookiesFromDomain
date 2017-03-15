14ceb72694b0e3fd787181668ae7ebca4a28627d
SeameX <info@seamex.de>
2017-03-15 20:17:21 +0100

    Added toplevel domains like localhost + small refactoring

--- js/main.js
+++ js/main.js
@@ -1,58 +1,44 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
-
     removeAllCookiesFromDomain(tab.url);
-    chrome.tabs.reload(tab.id);
 });
+
 
 function removeAllCookiesFromDomain(currentTabUrl) {
     var url = (new URL(currentTabUrl)).hostname;
-    // Don't split if domain is localhost
-    if (url != 'localhost') {
-      var splittedUrl = url.split('.');
-      var urlPieces = splittedUrl.pop(); // *.de
 
-      // Each time one more Subdomain
-      while (splittedUrl.length > 0) {
+    var splittedUrl = url.split('.');
+    var urlPieces = splittedUrl.pop(); // *.de
 
-          urlPieces = splittedUrl.pop() + "." + urlPieces; // first loop: seamex.de
+    // Remove toplevel domains aswell (like localhost)
+    if (splittedUrl.length == 0)
+        getAndRemoveCookiesForDomain(url);
 
-          // Without Dot
-          chrome.cookies.getAll({domain: urlPieces}, function (cookies) {
-              for (var i = 0; i < cookies.length; i++) {
-                  var scheme = "http://";
+    // Remove cookies for every subdomain
+    while (splittedUrl.length > 0) {
+        urlPieces = splittedUrl.pop() + "." + urlPieces;
+        getAndRemoveCookiesForDomain(urlPieces);
+    }
+}
 
-                  if(cookies[i].secure)
-                      scheme = "https://";
+function getAndRemoveCookiesForDomain(urlPieces) {
+    // Without Dot
+    chrome.cookies.getAll({domain: urlPieces}, function (cookies) {
+        removeCookies(urlPieces, cookies);
+    });
 
-                  // Protocol agnostic
-                  chrome.cookies.remove({url: scheme + urlPieces + cookies[i].path, name: cookies[i].name});
-              }
-          });
+    // With dot
+    chrome.cookies.getAll({domain: "." + urlPieces}, function (cookies) {
+        removeCookies(urlPieces, cookies);
+    });
+}
 
-          // With dot
-          chrome.cookies.getAll({domain: "." + urlPieces}, function (cookies) {
-              for (var i = 0; i < cookies.length; i++) {
-                  var scheme = "http://";
+function removeCookies(urlPieces, cookies) {
+    for (var i = 0; i < cookies.length; i++) {
+        var scheme = "http://";
 
-                  if(cookies[i].secure)
-                      scheme = "https://";
+        if (cookies[i].secure)
+            scheme = "https://";
 
-                  // Protocol agnostic
-                  chrome.cookies.remove({url: scheme + urlPieces + cookies[i].path, name: cookies[i].name});
-              }
-          });
-      }
-    } else {
-      // remove cookies for localhost
-      chrome.cookies.getAll({domain: "localhost"}, function (cookies) {
-          for (var i = 0; i < cookies.length; i++) {
-              var scheme = "http://";
-              if(cookies[i].secure)
-                  scheme = "https://";
-
-              // Protocol agnostic
-              chrome.cookies.remove({url: scheme + "localhost" + cookies[i].path, name: cookies[i].name});
-          }
-      });
+        chrome.cookies.remove({url: scheme + urlPieces + cookies[i].path, name: cookies[i].name});
     }
 }
